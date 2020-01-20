6d4de04efcc23ebae07621223d8333eb23fbb774
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-01-19 21:33:17 -0300

    Correção para o ambiente r-spatial

---
+++ install.R
@@ -0,0 +1,4 @@
+install.packages(c('sf', 'sp'), repos = "http://cran.us.r-project.org")
+
+install.packages('https://cran.r-project.org/src/contrib/Archive/rgdal/rgdal_1.3-1.tar.gz', 
+                    repos=NULL, type="source")
